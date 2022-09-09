4927a1e79908dfc91b804475c80a59f13ded5c04
J. Nick Koston <nick@koston.org>
2022-09-09 09:17:51 -0500

    chore: build fixes

--- src/dbus_fast/__version__.py
+++ src/dbus_fast/__version__.py
@@ -1,8 +1,10 @@
 __title__ = "dbus_fast"
-__description__ = "A zero-dependency DBus library for Python with asyncio support"
-__url__ = "https://github.com/altdesktop/python-dbus-fast"
-__version__ = "0.2.3"
-__author__ = "Tony Crisci"
-__author_email__ = "tony@dubstepdish.com"
+__description__ = (
+    "A performant zero-dependency DBus library for Python with asyncio support"
+)
+__url__ = "https://github.com/bluetooth-devices/dbus-fast"
+__version__ = "1.0.0"
+__author__ = "Bluetooth Devices authors, Tony Crisci"
+__author_email__ = "bluetooth@koston.org"
 __license__ = "MIT"
-__copyright__ = "Copyright 2019 Tony Crisci"
+__copyright__ = "Copyright 2022 Bluetooth Devices authors, 2019 Tony Crisci"
