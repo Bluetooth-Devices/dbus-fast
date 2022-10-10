cde1893dca1872d8b482a145337ee3bbf47c35b4
J. Nick Koston <nick@koston.org>
2022-10-10 13:13:34 -1000

    feat: add additional typing (#100)

--- src/dbus_fast/errors.py
+++ src/dbus_fast/errors.py
@@ -1,3 +1,6 @@
+from typing import Optional, Union
+
+
 class SignatureBodyMismatchError(ValueError):
     pass
 
@@ -31,22 +34,22 @@ class SignalDisabledError(Exception):
 
 
 class InvalidBusNameError(TypeError):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(f"invalid bus name: {name}")
 
 
 class InvalidObjectPathError(TypeError):
-    def __init__(self, path):
+    def __init__(self, path: str) -> None:
         super().__init__(f"invalid object path: {path}")
 
 
 class InvalidInterfaceNameError(TypeError):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(f"invalid interface name: {name}")
 
 
 class InvalidMemberNameError(TypeError):
-    def __init__(self, member):
+    def __init__(self, member: str) -> None:
         super().__init__(f"invalid member name: {member}")
 
 
@@ -56,13 +59,15 @@ from .validators import assert_interface_name_valid
 
 
 class DBusError(Exception):
-    def __init__(self, type_, text, reply=None):
+    def __init__(
+        self, type_: Union[ErrorType, str], text: str, reply: Optional[Message] = None
+    ) -> None:
         super().__init__(text)
 
         if type(type_) is ErrorType:
             type_ = type_.value
 
-        assert_interface_name_valid(type_)
+        assert_interface_name_valid(type_)  # type: ignore[arg-type]
         if reply is not None and type(reply) is not Message:
             raise TypeError("reply must be of type Message")
 
@@ -71,9 +76,9 @@ class DBusError(Exception):
         self.reply = reply
 
     @staticmethod
-    def _from_message(msg):
+    def _from_message(msg: Message) -> "DBusError":
         assert msg.message_type == MessageType.ERROR
-        return DBusError(msg.error_name, msg.body[0], reply=msg)
+        return DBusError(msg.error_name or "unknown", msg.body[0], reply=msg)
 
-    def _as_message(self, msg):
+    def _as_message(self, msg: Message) -> Message:
         return Message.new_error(msg, self.type, self.text)
