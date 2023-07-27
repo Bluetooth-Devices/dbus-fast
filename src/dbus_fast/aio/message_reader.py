24dc46cca28fdbab4c941eff5cd4cd09d13dd931
J. Nick Koston <nick@koston.org>
2023-07-27 15:22:12 -0500

    fix: only read one

--- src/dbus_fast/aio/message_reader.py
+++ src/dbus_fast/aio/message_reader.py
@@ -16,24 +16,20 @@ def build_message_reader(
 ) -> None:
     """Build a callable that reads messages from the unmarshaller and passes them to the process function."""
     unmarshaller = Unmarshaller(stream, sock)
-    _tell = stream.tell if not sock else None
 
     def _message_reader() -> None:
         """Reads messages from the unmarshaller and passes them to the process function."""
         try:
-            while True:
-                message = unmarshaller._unmarshall()
-                if not message:
-                    return
-                try:
-                    process(message)
-                except Exception as e:
-                    logging.error(
-                        "Unexpected error processing message: %s", e, exc_info=True
-                    )
-                unmarshaller._reset()
-                if _tell:
-                    logging.error("After read tell: %s", _tell())
+            message = unmarshaller._unmarshall()
+            if not message:
+                return
+            try:
+                process(message)
+            except Exception as e:
+                logging.error(
+                    "Unexpected error processing message: %s", e, exc_info=True
+                )
+            unmarshaller._reset()
         except Exception as e:
             finalize(e)
 
