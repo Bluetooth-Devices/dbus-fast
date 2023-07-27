72d6d67970aeb85f4cda3b65eeea0ef7c78f27b0
J. Nick Koston <nick@koston.org>
2023-07-27 15:32:41 -0500

    fix: back to original

--- src/dbus_fast/aio/message_reader.py
+++ src/dbus_fast/aio/message_reader.py
@@ -35,7 +35,7 @@ def build_message_reader(
             # stream.peek will try to do a read call if the buffer is empty
             # but we don't want that to happen but there is no public
             # API to check if the buffer is empty so we cannot fully optimize
-            # this case
+            # this case but it still better than calling _unmarshall() again
             if _peek and not _peek():
                 # No more data to read, avoid calling _unmarshall() again
                 # since we know it will raise an exception internally
