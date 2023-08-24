0ef212b3381d7c4f5b801b2c63433143b1f36d1e
J. Nick Koston <nick@koston.org>
2023-08-24 08:15:34 -0500

    fix: more debugging

--- tests/test_send_reply.py
+++ tests/test_send_reply.py
@@ -1,4 +1,7 @@
 import os
+import traceback
+from types import TracebackType
+from typing import Any, Callable, Dict, List, Optional, Type, Union
 from unittest.mock import Mock
 
 import pytest
@@ -9,7 +12,57 @@ from dbus_fast.message import Message
 from dbus_fast.message_bus import BaseMessageBus, SendReply
 
 
-def test_send_reply_exception():
+class InlineSendReply:
+    """A context manager to send a reply to a message."""
+
+    __slots__ = ("_bus", "_msg")
+
+    def __init__(self, bus: "BaseMessageBus", msg: Message) -> None:
+        """Create a new reply context manager."""
+        self._bus = bus
+        self._msg = msg
+
+    def __enter__(self):
+        return self
+
+    def __call__(self, reply: Message) -> None:
+        self._bus.send(reply)
+
+    def _exit(
+        self,
+        exc_type: Optional[Type[Exception]],
+        exc_value: Optional[Exception],
+        tb: Optional[TracebackType],
+    ) -> bool:
+        if exc_value:
+            if isinstance(exc_value, DBusError):
+                self(exc_value._as_message(self._msg))
+            else:
+                self(
+                    Message.new_error(
+                        self._msg,
+                        ErrorType.SERVICE_ERROR,
+                        f"The service interface raised an error: {exc_value}.\n{traceback.format_tb(tb)}",
+                    )
+                )
+            return True
+
+        return False
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[Exception]],
+        exc_value: Optional[Exception],
+        tb: Optional[TracebackType],
+    ) -> bool:
+        return self._exit(exc_type, exc_value, tb)
+
+    def send_error(self, exc: Exception) -> None:
+        self._exit(exc.__class__, exc, exc.__traceback__)
+
+
+@pytest.mark.parametrize("send_reply_class", (SendReply, InlineSendReply))
+def test_send_reply_exception(send_reply_class: Union[SendReply, InlineSendReply]):
     """Test that SendReply sends an error message when DBusError is raised."""
 
     messages = []
@@ -19,19 +72,22 @@ def test_send_reply_exception():
         def send(self, msg: Message) -> None:
             messages.append(msg)
 
+        def send_message(self, msg: Message) -> None:
+            messages.append(msg)
+
         def _setup_socket(self) -> None:
             pass
 
     mock_message_bus = MockBus()
     mock_message = Message(
-        path="/test/path", interface="test.interface", member="test_member"
+        path="/test/path", interface="test.interface", member="test_member", serial=1
     )
-    send_reply = SendReply(mock_message_bus, mock_message)
+    send_reply = send_reply_class(mock_message_bus, mock_message)
 
     with send_reply as reply:
         raise DBusError(ErrorType.DISCONNECTED, "Disconnected", None)
 
-    assert mock_message_bus.send_message.call_count == 1
-    assert (
-        mock_message_bus.send_message.call_args[0][0].message_type == MessageType.ERROR
-    )
+    assert len(messages) == 1
+    assert messages[0].message_type == MessageType.ERROR
+    assert messages[0].error_name == "org.freedesktop.DBus.Error.Disconnected"
+    assert messages[0].reply_serial == 1
