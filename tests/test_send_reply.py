65218df57b4fecf9cc5990d6295204f0eb3cc1dd
J. Nick Koston <nick@koston.org>
2023-08-24 08:10:22 -0500

    fix: add test

--- tests/test_send_reply.py
+++ tests/test_send_reply.py
@@ -1,3 +1,4 @@
+import os
 from unittest.mock import Mock
 
 import pytest
@@ -5,14 +6,26 @@ import pytest
 from dbus_fast.constants import ErrorType, MessageType
 from dbus_fast.errors import DBusError
 from dbus_fast.message import Message
-from dbus_fast.message_bus import SendReply
+from dbus_fast.message_bus import BaseMessageBus, SendReply
 
 
 def test_send_reply_exception():
     """Test that SendReply sends an error message when DBusError is raised."""
 
-    mock_message_bus = Mock()
-    mock_message = Mock()
+    messages = []
+    os.environ["DBUS_SESSION_BUS_ADDRESS"] = "unix:path=/dev/null"
+
+    class MockBus(BaseMessageBus):
+        def send(self, msg: Message) -> None:
+            messages.append(msg)
+
+        def _setup_socket(self) -> None:
+            pass
+
+    mock_message_bus = MockBus()
+    mock_message = Message(
+        path="/test/path", interface="test.interface", member="test_member"
+    )
     send_reply = SendReply(mock_message_bus, mock_message)
 
     with send_reply as reply:
