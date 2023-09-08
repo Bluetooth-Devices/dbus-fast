ca7a26cbc932bf378c6274dc00f4446cf38accc8
J. Nick Koston <nick@koston.org>
2023-09-08 18:17:24 -0500

    chore: cover

--- tests/test_address_parser.py
+++ tests/test_address_parser.py
@@ -21,7 +21,7 @@ def test_valid_addresses():
         "tcp:host=127.0.0.1,port=55556": [
             ("tcp", {"host": "127.0.0.1", "port": "55556"})
         ],
-        "unix:tmpdir=/tmp;": [("unix", {"tmpdir": "/tmp"})],
+        "unix:tmpdir=/tmp,;": [("unix", {"tmpdir": "/tmp"})],
     }
 
     for address, parsed in valid_addresses.items():
