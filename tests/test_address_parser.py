370791da869040d4a0d92cde30c4d2d4d684bcdc
J. Nick Koston <nick@koston.org>
2023-09-08 18:33:53 -0500

    fix: clean up address parsing and tests (#244)

--- tests/test_address_parser.py
+++ tests/test_address_parser.py
@@ -1,4 +1,16 @@
-from dbus_fast._private.address import parse_address
+import os
+from unittest.mock import patch
+
+import pytest
+
+from dbus_fast._private.address import (
+    get_bus_address,
+    get_session_bus_address,
+    get_system_bus_address,
+    parse_address,
+)
+from dbus_fast.constants import BusType
+from dbus_fast.errors import InvalidAddressError
 
 
 def test_valid_addresses():
@@ -21,7 +33,42 @@ def test_valid_addresses():
         "tcp:host=127.0.0.1,port=55556": [
             ("tcp", {"host": "127.0.0.1", "port": "55556"})
         ],
+        "unix:tmpdir=/tmp,;": [("unix", {"tmpdir": "/tmp"})],
     }
 
     for address, parsed in valid_addresses.items():
         assert parse_address(address) == parsed
+
+
+def test_invalid_addresses():
+    with pytest.raises(InvalidAddressError):
+        assert parse_address("")
+    with pytest.raises(InvalidAddressError):
+        assert parse_address("unix")
+    with pytest.raises(InvalidAddressError):
+        assert parse_address("unix:tmpdir")
+    with pytest.raises(InvalidAddressError):
+        assert parse_address("unix:tmpdir=😁")
+
+
+def test_get_system_bus_address():
+    with patch.dict(os.environ, DBUS_SYSTEM_BUS_ADDRESS="unix:path=/dog"):
+        assert get_system_bus_address() == "unix:path=/dog"
+        assert get_bus_address(BusType.SYSTEM) == "unix:path=/dog"
+    with patch.dict(os.environ, DBUS_SYSTEM_BUS_ADDRESS=""):
+        assert get_system_bus_address() == "unix:path=/var/run/dbus/system_bus_socket"
+
+
+def test_get_session_bus_address():
+    with patch.dict(os.environ, DBUS_SESSION_BUS_ADDRESS="unix:path=/dog"):
+        assert get_session_bus_address() == "unix:path=/dog"
+        assert get_bus_address(BusType.SESSION) == "unix:path=/dog"
+    with patch.dict(os.environ, DBUS_SESSION_BUS_ADDRESS="", DISPLAY=""), pytest.raises(
+        InvalidAddressError
+    ):
+        assert get_session_bus_address()
+
+
+def test_invalid_bus_address():
+    with pytest.raises(Exception):
+        assert get_bus_address(-1)
