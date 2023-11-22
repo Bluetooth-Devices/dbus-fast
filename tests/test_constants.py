c6a8301704162e1c4d07470c32ca0830f531b6d4
Stefan Agner <stefan@agner.ch>
2023-11-22 09:26:11 +0100

    feat: make ErrorType enums compare as strings (#269)
    The DBusError exception stores the error type as string. This makes the
    exception not directly compare to the ErrorType members (for example
    DBusError(ErrorType.FAILED, "").type != ErrorType.FAILED). This makes
    ErrorType also a string to make this comparision work.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,6 +1,12 @@
-from dbus_fast.constants import MESSAGE_FLAG_MAP, MessageFlag
+from dbus_fast.constants import MESSAGE_FLAG_MAP, ErrorType, MessageFlag
+from dbus_fast.errors import DBusError
 
 
 def test_message_flag_map():
     assert 0 in MESSAGE_FLAG_MAP
     assert MessageFlag.NONE in MESSAGE_FLAG_MAP
+
+
+def test_error_type():
+    err = DBusError(ErrorType.FAILED, "")
+    assert ErrorType.FAILED == err.type
