8ee18a1355b247e3ef9c7ad5f561d7cc8f9cf4a2
J. Nick Koston <nick@koston.org>
2023-12-03 16:49:49 -1000

    feat: small speed up to the aio message reader (#273)

--- src/dbus_fast/aio/message_reader.py
+++ src/dbus_fast/aio/message_reader.py
@@ -1,9 +1,37 @@
 import logging
 import socket
+from functools import partial
 from typing import Callable, Optional
 
 from .._private.unmarshaller import Unmarshaller
 from ..message import Message
+
+
+def _message_reader(
+    unmarshaller: Unmarshaller,
+    process: Callable[[Message], None],
+    finalize: Callable[[Optional[Exception]], None],
+    negotiate_unix_fd: bool,
+) -> None:
+    """Reads messages from the unmarshaller and passes them to the process function."""
+    try:
+        while True:
+            message = unmarshaller._unmarshall()
+            if message is None:
+                return
+            try:
+                process(message)
+            except Exception as e:
+                logging.error("Unexpected error processing message: %s", exc_info=True)
+            # If we are not negotiating unix fds, we can stop reading as soon as we have
+            # the buffer is empty as asyncio will call us again when there is more data.
+            if (
+                not negotiate_unix_fd
+                and not unmarshaller._has_another_message_in_buffer()
+            ):
+                return
+    except Exception as e:
+        finalize(e)
 
 
 def build_message_reader(
@@ -14,28 +42,4 @@ def build_message_reader(
 ) -> Callable[[], None]:
     """Build a callable that reads messages from the unmarshaller and passes them to the process function."""
     unmarshaller = Unmarshaller(None, sock, negotiate_unix_fd)
-
-    def _message_reader() -> None:
-        """Reads messages from the unmarshaller and passes them to the process function."""
-        try:
-            while True:
-                message = unmarshaller._unmarshall()
-                if message is None:
-                    return
-                try:
-                    process(message)
-                except Exception as e:
-                    logging.error(
-                        "Unexpected error processing message: %s", exc_info=True
-                    )
-                # If we are not negotiating unix fds, we can stop reading as soon as we have
-                # the buffer is empty as asyncio will call us again when there is more data.
-                if (
-                    not negotiate_unix_fd
-                    and not unmarshaller._has_another_message_in_buffer()
-                ):
-                    return
-        except Exception as e:
-            finalize(e)
-
-    return _message_reader
+    return partial(_message_reader, unmarshaller, process, finalize, negotiate_unix_fd)
