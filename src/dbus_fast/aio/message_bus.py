1ca8a941bdf4e557fb721e5a4e1444aaccf9d897
J. Nick Koston <nick@home-assistant.io>
2025-08-27 09:20:46 -0500

    chore: small fixes for python 3.9

--- src/dbus_fast/aio/message_bus.py
+++ src/dbus_fast/aio/message_bus.py
@@ -318,7 +318,9 @@ class MessageBus(BaseMessageBus):
         finally:
             timer_handle.cancel()
 
-    def introspect_sync(self, bus_name: str, path: str, *args, **kwargs) -> intr.Node:
+    def introspect_sync(
+        self, bus_name: str, path: str, *args: Any, **kwargs: Any
+    ) -> intr.Node:
         return self._loop.run_until_complete(
             self.introspect(bus_name, path, *args, **kwargs)
         )
