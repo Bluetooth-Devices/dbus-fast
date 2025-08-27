969d8beb02bbba64bd17e9890d60fdd341e2a08e
Matt Whitlock <matt@whitlock.name>
2025-08-27 07:50:17 -0400

    add unit tests of BaseProxyObject.get_children()
    These tests currently fail due to one bug and one deficiency in
    BaseProxyObject.get_children():
    
     * Child proxy objects are constructed using the parent object's path.
       Thus, all calls to methods or accesses to properties of the child
       object are erroneously directed to the parent object.
    
     * Child proxy objects are constructed using the child Node from the
       parent's introspection. However, the parent might not provide
       introspection of its children. The D-Bus specification says:
    
         “If a child <node> has any sub-elements, then they must represent a
          complete introspection of the child. If a child <node> is empty,
          then it may or may not have sub-elements; the child must be
          introspected in order to find out. The intent is that if an object
          knows that its children are "fast" to introspect it can go ahead
          and return their information, but otherwise it can omit it.”
    
       When the parent object omits introspection of a child object, the
       corresponding proxy object is erroneously constructed without any
       interfaces or children of its own.

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -1,4 +1,6 @@
 import os
+import pytest
+import xml.etree.ElementTree as ET
 
 from dbus_fast import (
     ArgDirection,
@@ -7,6 +9,10 @@ from dbus_fast import (
     SignatureType,
 )
 from dbus_fast import introspection as intr
+from dbus_fast.constants import ErrorType
+from dbus_fast.errors import DBusError
+from dbus_fast.message_bus import BaseMessageBus
+from dbus_fast.proxy_object import BaseProxyObject, BaseProxyInterface
 
 with open(f"{os.path.dirname(__file__)}/data/strict-introspection.xml") as f:
     strict_data = f.read()
@@ -145,3 +151,150 @@ def test_default_interfaces():
     # just make sure it doesn't throw
     default = intr.Node.default()
     assert type(default) is intr.Node
+
+
+class MockMessageBus(BaseMessageBus):
+
+    def __init__(self,
+        nodes: dict[str, dict[str, intr.Node]]
+    ) -> None:
+        super().__init__(ProxyObject=MockProxyObject)
+        self.nodes = nodes
+        self.introspect_count = 0
+        # disable bus name tracking for testing purposes
+        class MockNameOwners(dict):
+            def get(self, key, default):
+                return dict.get(self, key, default if default else ':')
+        self._name_owners = MockNameOwners()
+
+    def introspect_sync(self, bus_name: str, path: str) -> intr.Node:
+        self.introspect_count = self.introspect_count + 1
+        service = self.nodes.get(bus_name)
+        if service is None:
+            raise DBusError(ErrorType.NAME_HAS_NO_OWNER,
+                    f"unknown service: {bus_name}")
+        node = service.get(path)
+        if node is None:
+            raise DBusError(ErrorType.UNKNOWN_OBJECT,
+                    f"unknown object: {path}")
+        return node
+
+    def _setup_socket(self) -> None:
+        pass
+
+    def _init_high_level_client(self) -> None:
+        pass
+
+
+class MockProxyInterface(BaseProxyInterface):
+
+    def _add_method(self, intr_method: intr.Method) -> None:
+        pass
+
+    def _add_property(self, intr_property: intr.Property) -> None:
+        pass
+
+
+class MockProxyObject(BaseProxyObject):
+
+    def __init__(self,
+        bus_name: str,
+        path: str,
+        introspection: intr.Node | str | ET.Element,
+        bus: BaseMessageBus
+    ) -> None:
+        super().__init__(bus_name, path, introspection, bus, MockProxyInterface)
+
+
+@pytest.mark.xfail(reason='buggy BaseProxyObject.get_children', raises=AssertionError, strict=True)
+def test_inline_child():
+    bus = MockMessageBus({
+            'com.example': {
+                '/com/example/parent_object': intr.Node.parse(
+"""<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+    "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<node name="/com/example/parent_object">
+    <interface name="com.example.ParentInterface">
+        <method name="ParentMethod"/>
+    </interface>
+    <node name="child_object">
+        <interface name="com.example.ChildInterface">
+            <property name="ChildProperty" type="i"/>
+        </interface>
+    </node>
+</node>""")
+            }
+        })
+    introspection = bus.introspect_sync('com.example',
+            '/com/example/parent_object')
+    assert bus.introspect_count == 1
+
+    parent = bus.get_proxy_object('com.example', '/com/example/parent_object',
+            introspection)
+    assert parent.bus_name == 'com.example'
+    assert parent.path == '/com/example/parent_object'
+    assert parent.child_paths == [
+            '/com/example/parent_object/child_object'
+        ]
+    interface = parent.get_interface('com.example.ParentInterface')
+    assert interface.bus_name == 'com.example'
+    assert interface.path == '/com/example/parent_object'
+    assert [method.name for method in interface.introspection.methods] == [
+            'ParentMethod'
+        ]
+
+    child = next(iter(parent.get_children()))
+    assert child.bus_name == 'com.example'
+    assert child.path == '/com/example/parent_object/child_object'
+    interface = child.get_interface('com.example.ChildInterface')
+    assert [prop.name for prop in interface.introspection.properties] == [
+            'ChildProperty'
+        ]
+
+    # getting the inline child should not have required another introspection
+    assert bus.introspect_count == 1
+
+
+@pytest.mark.xfail(reason='buggy BaseProxyObject.get_children', raises=AssertionError, strict=True)
+def test_noninline_child():
+    obj0_node = intr.Node.parse(strict_data)
+    bus = MockMessageBus({
+            'com.example': {
+                obj0_node.name: obj0_node,
+                f'{obj0_node.name}/child_of_sample_object': intr.Node.parse(
+f"""<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+    "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<node name="{obj0_node.name}/child_of_sample_object">
+    <interface name="com.example.ChildInterface">
+        <property name="ChildProperty" type="i"/>
+    </interface>
+</node>"""),
+            }
+        })
+    introspection = bus.introspect_sync('com.example',
+            '/com/example/sample_object0')
+    assert bus.introspect_count == 1
+    parent = bus.get_proxy_object('com.example', '/com/example/sample_object0',
+            introspection)
+
+    assert parent.path == '/com/example/sample_object0'
+    assert parent.child_paths == [
+            '/com/example/sample_object0/child_of_sample_object',
+            '/com/example/sample_object0/another_child_of_sample_object',
+        ]
+    children = parent.get_children()
+    assert [child.path for child in children] == parent.child_paths
+
+    # merely listing the children should not have required another introspection
+    assert bus.introspect_count == 1
+
+    child = next(child for child in children if child.path == \
+            '/com/example/sample_object0/child_of_sample_object')
+    interface = child.get_interface('com.example.ChildInterface')
+
+    # obtaining the child interface should have required a second introspection
+    assert bus.introspect_count == 2
+
+    assert [prop.name for prop in interface.introspection.properties] == [
+            'ChildProperty'
+        ]
